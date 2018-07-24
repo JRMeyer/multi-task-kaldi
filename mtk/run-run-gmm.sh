609d53e763749a662c3ca00d94f6ea6e2659c59d
Ubuntu <ubuntu@ip-172-31-29-116.us-west-2.compute.internal>
2018-07-24 12:40:11 +0300

    top run scripts

---
+++ mtk/run-run-gmm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./run_gmm.sh atai-100A-mod 01
+./run_gmm.sh atai-100B-mod 01
+./run_gmm.sh atai-100C-mod 01
+./run_gmm.sh atai-100D-mod 01
+./run_gmm.sh atai-100E-mod 01
+./run_gmm.sh atai-100F-mod 01
