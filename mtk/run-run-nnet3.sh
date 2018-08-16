a92033ce7cc3f74e34262dc3fdf946e5167d7a25
Josh Meyer <joshuameyer@email.arizona.edu>
2018-08-16 06:05:30 -0500

    local

--- mtk/run-run-nnet3.sh
+++ mtk/run-run-nnet3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dim=768
-num_epochs=10
+dim=512
+num_epochs=2
 main_dir=MTL
 
 
@@ -28,24 +28,24 @@ main_dir=MTL
 rm -rf MTL/exp/nnet3
 ./run_nnet3_multitask.sh "atai-100A-org" "tri" "1.0" $dim $num_epochs $main_dir
 
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100B-org" "tri" "1.0" $dim $num_epochs $main_dir
-
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100C-org" "tri" "1.0" $dim $num_epochs $main_dir
-
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100D-org" "tri" "1.0" $dim $num_epochs $main_dir
-
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100E-org" "tri" "1.0" $dim $num_epochs $main_dir
-
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100F-org" "tri" "1.0" $dim $num_epochs $main_dir
-
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100B-org" "tri" "1.0" $dim $num_epochs $main_dir
 
 rm -rf MTL/exp/nnet3
-./run_nnet3_multitask.sh "atai-100A-mod" "tri" "1.0" $dim $num_epochs $main_dir
+./run_nnet3_multitask.sh "atai-100C-org" "tri" "1.0" $dim $num_epochs $main_dir
+
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100D-org" "tri" "1.0" $dim $num_epochs $main_dir
+
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100E-org" "tri" "1.0" $dim $num_epochs $main_dir
+
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100F-org" "tri" "1.0" $dim $num_epochs $main_dir
+
+
+# rm -rf MTL/exp/nnet3
+# ./run_nnet3_multitask.sh "atai-100A-mod" "tri" "1.0" $dim $num_epochs $main_dir
 
 # rm -rf MTL/exp/nnet3
 # ./run_nnet3_multitask.sh "atai-100B-mod" "tri" "1.0" $dim $num_epochs $main_dir
@@ -67,20 +67,20 @@ rm -rf MTL/exp/nnet3
 rm -rf MTL/exp/nnet3
 ./run_nnet3_multitask.sh "atai-100A-org atai-100A-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
 
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100B-org atai-100B-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100B-org atai-100B-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
 
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100C-org atai-100C-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100C-org atai-100C-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
 
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100D-org atai-100D-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100D-org atai-100D-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
 
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100E-org atai-100E-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100E-org atai-100E-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
 
-# rm -rf MTL/exp/nnet3
-# ./run_nnet3_multitask.sh "atai-100F-org atai-100F-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
+rm -rf MTL/exp/nnet3
+./run_nnet3_multitask.sh "atai-100F-org atai-100F-mod" "tri tri" "0.8,0.2" $dim $num_epochs $main_dir
 
 
 sudo shutdown now
