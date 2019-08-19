4d83f5aad3c89f0c7df9d703209ca70ad76e7fdb
Josh Meyer <JRMeyer@users.noreply.github.com>
2019-08-18 21:22:28 -0700

    nnet == flag
    simple if condition for nnet2 vs nnet3, some formatting

--- mtk/utils/format_accuracy_for_plot.sh
+++ mtk/utils/format_accuracy_for_plot.sh
@@ -4,59 +4,48 @@
 ## compute_prob_train.* so that the scipt utils/plot_accuracy.py
 ## and matplotlib can work with it.
 
+LOGDIR=$1
+NNET=$2
+OUTPUT=$3
 
-
-logdir=$1
-save_to=$2
-
-## NNET2 ##
-
-for i in ${logdir}/compute_prob_valid.*; do
-    trial=(${i//./ });
-    trial=${trial[1]}
-    
-    grep -oP 'accuracy is [0-9]\.[0-9]+' $i | while read -r match; do
-        myarray=($match);
-        echo "valid" "'output-0'" "$trial" "${myarray[2]}" >> $save_to;
+if [ $NNET == "nnet2" ]; then
+    ## NNET2 ##
+    # loop over validation data logs
+    for i in ${LOGDIR}/compute_prob_valid.*; do
+        trial=(${i//./ });
+        trial=${trial[1]};
+        grep -oP 'accuracy is [0-9]\.[0-9]+' $i | while read -r match; do
+            myarray=($match);
+            echo "valid" "'output-0'" "$trial" "${myarray[2]}" >> $OUTPUT;
+        done
     done
-done
-
-
-for i in ${logdir}/compute_prob_train.*; do
-    trial=(${i//./ });
-    trial=${trial[1]}
-    
-    grep -oP 'accuracy is [0-9]\.[0-9]+' $i | while read -r match; do
-        myarray=($match);
-        echo "train" "'output-0'" "$trial" "${myarray[2]}" >> $save_to;
+    # loop over training data logs
+    for i in ${LOGDIR}/compute_prob_train.*; do
+        trial=(${i//./ });
+        trial=${trial[1]};
+        grep -oP 'accuracy is [0-9]\.[0-9]+' $i | while read -r match; do
+            myarray=($match);
+            echo "train" "'output-0'" "$trial" "${myarray[2]}" >> $OUTPUT;
+        done
     done
-done
-
-
-
-
-## NNET3 ##
-
-# for i in ${logdir}/compute_prob_valid.*; do
-#     trial=(${i//./ });
-#     trial=${trial[1]};
-
-#     grep -oP '(?<=accuracy for).+(?= per frame)' $i | while read -r match; do
-#         myarray=($match);
-#         echo "valid" "${myarray[0]}" "$trial" "${myarray[2]}" >> $save_to;
-#     done
-# done
-
-
-# for i in ${logdir}/compute_prob_train.*; do
-#     trial=(${i//./ });
-#     trial=${trial[1]};
-
-#     grep -oP '(?<=accuracy for).+(?= per frame)' $i | while read -r match; do
-#         myarray=($match);
-#         echo "train" "${myarray[0]}" "$trial" "${myarray[2]}" >> $save_to;
-#     done
-# done
-
-
-
+elif [ $NNET == "nnet3" ]; then
+    ## NNET3 ##
+    # loop over validation data logs
+    for i in ${LOGDIR}/compute_prob_valid.*; do
+        trial=(${i//./ });
+        trial=${trial[1]}    
+        grep -oP '(?<=accuracy for).+(?= per frame)' $i | while read -r match; do
+            myarray=($match);
+            echo "valid" "${myarray[0]}" "$trial" "${myarray[2]}" >> $OUTPUT;
+        done
+    done
+    # loop over training data logs
+    for i in ${LOGDIR}/compute_prob_train.*; do
+        trial=(${i//./ });
+        trial=${trial[1]};
+        grep -oP '(?<=accuracy for).+(?= per frame)' $i | while read -r match; do
+            myarray=($match);
+            echo "train" "${myarray[0]}" "$trial" "${myarray[2]}" >> $OUTPUT;
+        done
+    done
+fi
